cf6e781c8e98a16fdd5c2e003b99007d5d6a8a5e
isslayne <isslyne@gmail.com>
2021-08-13 14:25:03 +0800

    feat: swiftui demo

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Layne on 2021/8/13.
+//  Copyright © 2021 Jérémy Marchand. All rights reserved.
+//
+
+import SwiftUI
+import TVVLCPlayer
+import TVVLCKit
+
+struct TestTVVLCPlayerController: UIViewControllerRepresentable {
+    typealias UIViewControllerType = VLCPlayerViewController
+    
+    func makeUIViewController(context: Context) -> VLCPlayerViewController {
+        let player = VLCMediaPlayer()
+        let media: VLCMedia = VLCMedia(url: URL(string: "https://upload.wikimedia.org/wikipedia/commons/8/88/Big_Buck_Bunny_alt.webm")!)
+        media.addOption(":http-user-agent=AppleCoreMedia/1.0.0.18L191 (Apple TV; U; CPU OS 14_5 like Mac OS X; zh_cn)")
+        player.media = media
+        let playerViewController = VLCPlayerViewController.instantiate(player: player)
+        return playerViewController
+    }
+    func updateUIViewController(_ uiViewController: VLCPlayerViewController, context: Context) {
+    }
+}
+
+struct TestPlayerView: View {    
+    var body: some View {
+        TestTVVLCPlayerController()
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView: View {
+    var body: some View {
+        VStack {
+            NavigationLink(
+            destination: TestPlayerView()
+            ) {
+                Image(systemName: "folder.fill")
+                    .font(.system(size: 120))
+                    .foregroundColor(.yellow)
+                    .frame(width: 240, height: 360)
+                    .background(Color.white.opacity(0.2))
+                    .cornerRadius(12)
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            TabView {
+                SearchView()
+                    .tabItem {
+                        HStack {
+                            Text("搜索")
+                            Image(systemName: "magnifyingglass")
+                                .font(.system(size: 60))
+                        }
+                    }
+            }
+            .padding(.top, -60)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Layne on 2021/8/13.
+//  Copyright © 2021 Jérémy Marchand. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
